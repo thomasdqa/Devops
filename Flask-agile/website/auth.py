ef92bbd1ef61dbbd3ce62b21b13e4b1e18c14792
thomasd <thomasdmcrldn2020@gmail.com>
2023-04-03 12:45:11 +0100

    auth changes/ bug fix

--- Flask-agile/website/auth.py
+++ Flask-agile/website/auth.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
-from.
 from flask_login import login_user, login_required, logout_user, current_user
 from flask_bcrypt import Bcrypt
 import bleach
